6a19cf427a2f891bbc7ca3b6a80758336fd67b16
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-11-06 00:07:13 +0300

    MongoDB & Mongoose
    Подключение и настройка MongoDB и Mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
@@ -25,8 +26,19 @@ app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
 app.use('/card', cardRoutes)
 
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+async function start() {
+  try {
+    const url = 'mongodb+srv://AqVadPlay:sWpyPeH2vvqfAEOd@cluster0.yb8wm.mongodb.net/Cluster0?retryWrites=true&w=majority';
+    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch(e) {
+    console.log(e);
+  }
+};
+
+start();
