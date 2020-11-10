5e800e79b7c05a19e3b5ffe3b2f63d8a9e935afb
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-11-10 08:07:52 +0300

    Удаление курса
    Реализован метод удаления курса

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ const PORT = process.env.PORT || 3000;
 async function start() {
   try {
     const url = 'mongodb+srv://AqVadPlay:sWpyPeH2vvqfAEOd@cluster0.yb8wm.mongodb.net/shop';
-    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect(url, {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
