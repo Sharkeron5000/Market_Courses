8a7ff5fa5871de75212d38cb90183f1f7352be7f
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-11-10 22:02:25 +0300

    Модель пользователя
    Добавлена модель пользователя

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  cart: {
+    item: [
+      {
+        count: {
+          type: Number,
+          required: true,
+          default: 1
+        },
+        courseId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Course',
+          required: true
+        }
+      }
+    ]
+  }
+});
+
+module.exports = model('User', userSchema);
