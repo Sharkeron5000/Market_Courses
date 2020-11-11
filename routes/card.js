753d574f8244b76c6e0b581e38e73a89ee368e1d
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-11-11 22:48:20 +0300

    Revert "Отображение корзины"
    This reverts commit 2f807bbc74ebe18de17f20a1cfbdfbba26df7c22.

--- routes/card.js
+++ routes/card.js
@@ -1,18 +1,6 @@
-const { Router } = require('express');
+const {Router} = require('express');
 const Course = require('../models/course');
 const router = Router();
-
-function mapCartItems(cart) {
-  return cart.items.map(c => ({
-    ...c.courseId._doc, count: c.count
-  }))
-};
-
-function computePrice(courses) {
-  return courses.reduce((total, course)=>{
-    return total += course.price * course.count;
-  }, 0)
-}
 
 router.post('/add', async (req, res) => {
   const course = await Course.findById(req.body.id);
@@ -31,19 +19,15 @@ router.delete('/remove/:id', async (req, res) => {
   res.status(200).json(cart);
 })
 
-router.get('/', async (req, res) => {
-  const user = await req.user
-    .populate('cart.items.courseId')
-    .execPopulate();
-
-  const courses = mapCartItems(user.cart)
-
-  res.render('card', {
-    title: 'Корзина',
-    isCard: true,
-    courses: courses,
-    price: computePrice(courses),
-  })
+router.get ('/', async (req, res) => {
+  // const card = await Card.fetch();
+  // res.render('card', {
+  //   title: 'Корзина',
+  //   isCard: true,
+  //   courses: card.courses,
+  //   price: card.price,
+  // })
+  res.json({test:true})
 })
 
 module.exports = router;
