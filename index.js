397e199f5151b5472a39eea619b1605279726c19
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-11-18 08:10:08 +0300

    Сессия
    Сохранение сессии

--- index.js
+++ index.js
@@ -26,15 +26,15 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('5fab13d0818a5104ec1e64fc');
-    req.user = user;
-    next();
-  } catch (e) {
-    console.log(e);
-  }
-})
+// app.use(async (req, res, next) => {
+//   try {
+//     const user = await User.findById('5fab13d0818a5104ec1e64fc');
+//     req.user = user;
+//     next();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// })
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -64,15 +64,15 @@ async function start() {
       useFindAndModify: false,
     });
 
-    const candidate = await User.findOne();
-    if (!candidate) {
-      const user = new User({
-        email: 'AqVadPlay@yandex.ru',
-        name: 'EliBro',
-        cart: { items: [] }
-      });
-      await user.save();
-    };
+    // const candidate = await User.findOne();
+    // if (!candidate) {
+    //   const user = new User({
+    //     email: 'AqVadPlay@yandex.ru',
+    //     name: 'EliBro',
+    //     cart: { items: [] }
+    //   });
+    //   await user.save();
+    // };
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
