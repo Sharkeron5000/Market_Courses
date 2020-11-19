b1e7f3cc706ca54415b1657c92e8eeda566635db
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-11-20 00:26:38 +0300

    Сообщения об ошибках
    добавлены сообщения с ошибками, во время регистрации/авторизации

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const csrf = require('csurf');
+const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
@@ -13,7 +14,6 @@ const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
 const orderRoutes = require('./routes/order');
 const authRoutes = require('./routes/auth');
-const User = require('./models/user');
 const varMiddleware = require('./middleaware/variables');
 const userMiddleware = require('./middleaware/user')
 
@@ -53,6 +53,7 @@ app.use(session({
   store,
 }));
 app.use(csrf());
+app.use(flash());
 app.use(varMiddleware);
 app.use(userMiddleware);
 
