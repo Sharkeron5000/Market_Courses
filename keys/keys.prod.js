c64f60d699a59703d28ee244ba649d220b65914c
Sharkeron5000 <Sharkeron5000@yandex.ru>
2020-12-06 23:27:26 +0300

    Деплой
    Подготовка к деплою.

---
+++ keys/keys.prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGODB_IRI: process.env.MONGODB_IRI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  BASE_URL: process.env.BASE_URL
+} 
